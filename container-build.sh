9295e1f3d17cb4896c1a5284bfc341b4b07ccd1a
Børge Moe <borge.moe@met.no>
2023-07-19 14:56:28 +0200

    Fix the container build

--- container-build.sh
+++ container-build.sh
@@ -7,17 +7,20 @@ kvuserid=5010
 mode=test
 targets=norcom2kv
 tag=latest
+tag_and_latest="false"
 os=focal
-registry="registry.met.no/obs/kvalobs/kvbuild"
+registry="registry.met.no/met/obsklim/kvalobs/kvbuild"
 nocache=
 only_build=
+VERSION="$(./version.sh)"
 
 gitref=$(git rev-parse --show-toplevel)/gitref.sh
 
 use() {
 
   usage="\
-Usage: $0 [--help] [--os os] [--staging|--prod|--test] [--tag tag] [--no-cache] [--only-build] target-list
+Usage: $0 [--help] [--os os] [--staging|--prod|--test] [--tag tag] [--no-cache] [--only-build] 
+       [--tag-and-latest tag] [--tag-version] target-list
 
 This script build a norcom2kv container. 
 Stop at build stage 'stage'. Default $targets.
@@ -31,6 +34,8 @@ Options:
   --help        display this help and exit.
   --os os       The os to build for, default $os. 
   --tag tagname tag the image with the name tagname, default $tag.
+  --tag-and-latest tagname tag the image with the name tagname  and also create latest tag.
+  --tag-version Use version from configure.ac as tag. Also tag latest.
   --staging     build and push to staging.
   --prod        build and push to prod.
   --test        only build. Default.
@@ -46,6 +51,13 @@ echo -e "$usage\n\n"
 while test $# -ne 0; do
   case $1 in
     --tag) tag=$2; shift;;
+    --tag-and-latest) 
+        tag="$2"
+        tag_and_latest=true
+        shift;;
+    --tag-version) 
+        tag="$VERSION"
+        tag_and_latest=true;;
     --help) 
         use
         exit 0;;
@@ -66,6 +78,7 @@ while test $# -ne 0; do
   shift
 done
 
+echo "VERSION: $VERSION"
 echo "tag: $tag"
 echo "mode: $mode"
 echo "os: $os"
@@ -89,8 +102,15 @@ for target in $targets ; do
   docker build $nocache --target $target --build-arg "kvuser=$kvuser" --build-arg "kvuserid=$kvuserid" \
     -f docker/${os}/${target}.dockerfile ${only_build} --tag ${registry}${target}:$tag .
   
+  if [ "$tag_and_latest" = "true" ]; then
+      docker tag "${registry}${target}:$tag" "${registry}${target}:latest"
+  fi
+  
   if [ $mode != test ]; then 
     docker push ${registry}${target}:$tag
+    if [ "$tag_and_latest" = "true" ]; then
+      docker push "${registry}${target}:latest"
+    fi
   fi
 done
 
