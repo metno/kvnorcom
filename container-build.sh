ef46f6d6b80b0048c67411871129fe3823a1785c
Børge Moe <borge.moe@met.no>
2024-03-20 10:50:54 +0100

    Build changes

--- container-build.sh
+++ container-build.sh
@@ -4,15 +4,17 @@ export DOCKER_BUILDKIT=1
 
 kvuser=kvalobs
 kvuserid=5010
-mode=test
-targets=norcom2kv
+mode="test"
+targets="norcom2kv"
 tag=latest
 tag_and_latest="false"
 os=focal
-registry="registry.met.no/met/obsklim/kvalobs/kvbuild"
+registry="registry.met.no/met/obsklim/bakkeobservasjoner/data-og-kvalitet/kvalobs/kvbuild"
 nocache=
-only_build=
+push="true"
+build="true"
 VERSION="$(./version.sh)"
+BUILDDATE=$(date +'%Y%m%d')
 
 gitref=$(git rev-parse --show-toplevel)/gitref.sh
 
@@ -20,10 +22,9 @@ use() {
 
   usage="\
 Usage: $0 [--help] [--os os] [--staging|--prod|--test] [--tag tag] [--no-cache] [--only-build] 
-       [--tag-and-latest tag] [--tag-version] target-list
+       [--tag-and-latest tag] [--tag-version] [--tag-with-build-date]
 
 This script build a norcom2kv container. 
-Stop at build stage 'stage'. Default $targets.
 
 If --staging or --prod is given it is copied to the 
 container registry at $registry. 
@@ -36,12 +37,16 @@ Options:
   --tag tagname tag the image with the name tagname, default $tag.
   --tag-and-latest tagname tag the image with the name tagname  and also create latest tag.
   --tag-version Use version from configure.ac as tag. Also tag latest.
+  --tag-with-build-date 
+                tag with version and build date on the form version-YYYYMMDD 
+                and set latest.
   --staging     build and push to staging.
   --prod        build and push to prod.
   --test        only build. Default.
-  --stage stage stop at build stage. Default $targets.
   --no-cache    Do not use the docker build cache.
   --only-build  Stop after building.
+  --only-push   Only push to registry. Must use the same flags as when building.
+
   
 "
 echo -e "$usage\n\n"
@@ -54,22 +59,30 @@ while test $# -ne 0; do
     --tag-and-latest) 
         tag="$2"
         tag_and_latest=true
-        shift;;
+        shift
+        ;;
     --tag-version) 
         tag="$VERSION"
-        tag_and_latest=true;;
+        tag_and_latest=true
+        ;;
+    --tag-with-build-date)
+        tag="$VERSION-$BUILDDATE"
+        tag_and_latest=true
+        ;;
     --help) 
         use
         exit 0;;
     --os)
         os=$2
         shift;;
-    --staging) mode=staging;;
-    --prod) mode=prod;;
-    --test) mode=test;;
+    --staging) mode="staging";;
+    --prod) mode="prod";;
+    --test) mode="test";;
     --no-cache) nocache="--no-cache";;
     --only-build)
-        only_build="--target build";;
+        push="false";;
+    --only-push)
+        build="true";;
     -*) use
       echo "Invalid option $1"
       exit 1;;  
@@ -82,7 +95,8 @@ echo "VERSION: $VERSION"
 echo "tag: $tag"
 echo "mode: $mode"
 echo "os: $os"
-echo "Build mode: $mode"
+echo "Build: $build"
+echo "Push: $push"
 echo "targets: $targets"
 
 if [ "$mode" = "test" ]; then 
@@ -99,16 +113,18 @@ echo "tag: $tag"
 $gitref 
 
 for target in $targets ; do
-  docker build $nocache --target $target --build-arg "kvuser=$kvuser" --build-arg "kvuserid=$kvuserid" \
-    -f docker/${os}/${target}.dockerfile ${only_build} --tag ${registry}${target}:$tag .
+  if [ "$build" = "true" ]; then
+    docker build $nocache --target $target --build-arg "kvuser=$kvuser" --build-arg "kvuserid=$kvuserid" \
+      -f docker/${os}/${target}.dockerfile --tag ${registry}${target}:$tag .
   
-  if [ "$tag_and_latest" = "true" ]; then
-      docker tag "${registry}${target}:$tag" "${registry}${target}:latest"
+    if [ "$tag_and_latest" = "true" ] && [ "${tag}" != "latest" ]; then
+        docker tag "${registry}${target}:$tag" "${registry}${target}:latest"
+    fi
   fi
   
-  if [ $mode != test ]; then 
+  if [ $mode != test ] && [ "$push" = "true" ]; then 
     docker push ${registry}${target}:$tag
-    if [ "$tag_and_latest" = "true" ]; then
+    if [ "$tag_and_latest" = "true" ] && [ "${tag}" != "latest" ]; then
       docker push "${registry}${target}:latest"
     fi
   fi
--- docker/focal/norcom2kv.dockerfile
+++ docker/focal/norcom2kv.dockerfile
@@ -1,8 +1,3 @@
-#syntax=docker/dockerfile:1.2
-
-#Build from the top source directory as
-# DOCKER_BUILDKIT=1 docker build -t norcom2kv -f docker/norcom2kv.dockerfile .
-
 FROM ubuntu:focal AS build
 
 RUN apt-get update && apt-get install -y libgmock-dev language-pack-nb-base\
@@ -12,14 +7,6 @@ RUN apt-get update && apt-get install -y libgmock-dev language-pack-nb-base\
 COPY docker/internrepo-4E8A0C14.asc /tmp/
 RUN apt-key add /tmp/internrepo-4E8A0C14.asc && rm /tmp/internrepo-4E8A0C14.asc && \
   add-apt-repository 'deb [arch=amd64] http://internrepo.met.no/focal focal main contrib'
-
-
-# RUN apt-get update && apt-get --yes \
-#   debhelper autotools-dev lsb-base g++  debconf automake libtool \
-#    libboost-dev libboost-thread-dev libboost-regex-dev libboost-filesystem-dev \
-#   libboost-filesystem-dev libboost-program-options-dev libomniorb4-dev \
-#   omniidl metlibs-putools-dev libkvcpp-dev 
-
 
 RUN apt-get update && apt-get install --yes \
   debhelper autotools-dev autoconf-archive libboost-thread-dev lsb-base g++ devscripts debconf automake libtool fakeroot \
